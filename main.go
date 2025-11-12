66cc56898b2e1b3ab2462592a7032bbac037728e
root <root@Cryptop.localdomain>
2025-11-12 15:08:25 -0800

    added users table to db, endpoint for user creation

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
-
+import _ "github.com/lib/pq"
 import (
+	"github.com/cryptidcodes/chirpy/internal/database"
 	"sync/atomic"
 	"fmt"
 	"net/http"
+	"os"
+	"database/sql"
+	"log"
+	"github.com/joho/godotenv"
 )
 
 type apiConfig struct {
 		fileserverHits atomic.Int32
+		dbQueries *database.Queries
+		platform string
 	}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	// load environment variables from .env file
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
+	// connect to the database
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil{
+		log.Fatal("Error connecting to the database: ", err)
+	}
+
+	dbQueries := database.New(db)
+
 	// init config struct
 	cfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries: dbQueries,
+		platform: platform,
 	}
 
 	// create a new http.ServeMux to handle requests
@@ -28,7 +56,8 @@ func main() {
 
 	// additional endpoint handlers
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/validate_chirp", cfg.handlerCreateChirp)
+	mux.HandleFunc("POST /api/users", cfg.handlerCreateUser)
 
 	// admin endpoint handlers
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
@@ -45,5 +74,4 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println("Server error:", err)
 	}
-
 }
